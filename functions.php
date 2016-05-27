fd753fa63ce09b51df201b7267a2302b71aac435
ihorbiletskyi <ihor.biletskyi@nure.ua>
2016-05-28 01:11:10 +0300

    Динамическое заполнение списка этажей и комнат

--- functions.php
+++ functions.php
@@ -41,6 +41,32 @@ function res($var)
     return $connection->real_escape_string($var);
 }
 
+function makeRoomsList()
+{
+
+    $res_floor = queryMysql("Select `ID` FROM `Floor`");
+    while ($row = mysqli_fetch_array($res_floor)) {
+        echo "       <div class=\"panel panel-default\">\n";
+        echo "      <div class=\"panel-heading\">\n";
+        echo "                   <h4 class=\"panel-title\">\n";
+        echo "                      <a data-toggle=\"collapse\" data-parent=\"#accordion\" href=\"#collapse".$row['ID']."\">Этаж " . $row['ID']."</a>\n";
+        echo "                   </h4>\n";
+        echo "               </div>\n";
+        $res_room = queryMysql("Select `ID` From `Room` Where `floor` =" .$row['ID']);
+        echo "        <div id=\"collapse".$row['ID']."\" class=\"panel-collapse collapse\">\n";
+        echo '                   <div class="list-group">'."\n";
+        while ($row_room = mysqli_fetch_array($res_room)) {
+            echo '<a href="'."room.php?id=".$row_room['ID'].'" class="list-group-item">Комната '. $row_room['ID']. '</a>'."\n";
+        }
+        echo "            </div>\n";
+        echo "            </div>\n";
+        echo "            </div>\n";
+    }
+    echo <<< _LIST
+
+_LIST;
+}
+
 /*--------------------------------------------------------------------------------------------*/
 function makeFooter()
 {
--- index.php
+++ index.php
@@ -1,58 +1,6 @@
 <?php require_once 'header.php'; ?>
     <div class="col-md-3">
-        <div class="panel-group" id="accordion">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h4 class="panel-title">
-                        <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">Этаж 1</a>
-                    </h4>
-                </div>
-                <div id="collapseOne" class="panel-collapse collapse in">
-                    <ul class="list-group">
-                        <li class="list-group-item"><a href="#">Комната 101</a></li>
-                        <li class="list-group-item"><a href="#">Комната 102</a></li>
-                        <li class="list-group-item"><a href="#">Комната 103</a></li>
-                        <li class="list-group-item"><a href="#">Комната 104</a></li>
-                        <li class="list-group-item"><a href="#">Комната 105</a></li>
-                        <li class="list-group-item"><a href="#">Комната 106</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h4 class="panel-title">
-                        <a data-toggle="collapse" data-parent="#accordion" href="#collapseFour">Этаж 2</a>
-                    </h4>
-                </div>
-                <div id="collapseFour" class="panel-collapse collapse">
-                    <div class="list-group">
-                        <a href="#" class="list-group-item">Комната 201</a>
-                        <a href="#" class="list-group-item">Комната 202</a>
-                        <a href="#" class="list-group-item">Комната 203</a>
-                        <a href="#" class="list-group-item">Комната 204</a>
-                        <a href="#" class="list-group-item">Комната 205</a>
-                        <a href="#" class="list-group-item">Комната 206</a>
-                    </div>
-                </div>
-            </div>
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h4 class="panel-title">
-                        <a data-toggle="collapse" data-parent="#accordion" href="#collapseFive">Этаж 3</a>
-                    </h4>
-                </div>
-                <div id="collapseFive" class="panel-collapse collapse">
-                    <div class="list-group">
-                        <a href="#" class="list-group-item">Комната 301</a>
-                        <a href="#" class="list-group-item">Комната 302</a>
-                        <a href="#" class="list-group-item">Комната 303</a>
-                        <a href="#" class="list-group-item">Комната 304</a>
-                        <a href="#" class="list-group-item">Комната 305</a>
-                        <a href="#" class="list-group-item">Комната 306</a>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <?php makeRoomsList()?>
     </div>
     <div class="col-md-9">
         <h3>Этаж 1</h3>
